f2648de175bd3074d70691c89cadf556cba81e34
Rik <Rikvstraaten@gmail.com>
2018-11-28 15:44:22 +0100

    Added splash page, fixed basic routing

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
