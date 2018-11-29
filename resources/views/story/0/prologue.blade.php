629690d0c2a3b5c53aa56ba2c6365e55a9cc4efd
Dorieke Berends <dorieke90@gmail.com>
2018-11-29 10:11:21 +0100

    Added name field + button to prologue

--- resources/views/story/0/prologue.blade.php
+++ resources/views/story/0/prologue.blade.php
@@ -4,6 +4,13 @@
 <p>Prologue</p>
 
 
+<div id="change-div">
+    <p>Hallo! Wat is jouw naam?</p>
+    <input name="name" type="text" class="text">
+    <button class="btn btn-primary" onclick="checkName()">Bevestig</button>
+</div>
+
+
 
 <p><a href="/chapters/1a">Choice A</a></p>
 
