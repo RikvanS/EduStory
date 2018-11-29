75a9aab67077bcacf15c01d504b566718d15a09f
Rik <Rikvstraaten@gmail.com>
2018-11-29 14:49:51 +0100

    Pushing updated index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,9 @@ Placeholder.
     @endif
 </li>
 @else
+<li class="nav-item">
+    <a class="nav-link" href="/chapters/prologue">{{ __('Proloog') }}</a>
+</li>
 <li class="nav-item dropdown">
     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
         {{ Auth::user()->name }} <span class="caret"></span>
