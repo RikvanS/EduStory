ab42104e5b61fff8213fe16e4bab1ac95ccde977
Falco Rodenburg <falcorodenburg@gmail.com>
2018-12-07 11:09:59 +0100

    splash page gestyled

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,36 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<link href="{{ asset('css/infomodal.css') }}" rel="stylesheet">
+<link href="{{ asset('css/splash.css') }}" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=MedievalSharp|Open+Sans|Almendra|Merriweather" rel="stylesheet">
 
-<i class="fas fa-info-circle" id="infobutton"></i>
+<div class="headtitle">
+    <h1 id="title">EduStory<h1>
+    <h2 id="subtitle">Het Hanze avontuur</h2>
+</div>
+<button id="infobutton"><img src="../images/info.svg" id="info-icon"></button>
 <div id="infomodal" class="modal">
     <div class="modal-content">
-            <h1 id="info-title">Info</h1>
-            <p>Info tekst gebied</p>
+            <h2 id="info-title">Over Edustory</h2>
+            <p>EduStory is een educatief en interactief verhaal, gericht op basisschool kinderen van groep 7 en 8.
+   Deze website is vrij om te gebruiken, voor beide het gebruik thuis alsmede op school.
+   Als richtlijnen houden wij bij onze stof de leerlijnen van Nationaal Expertisecentrum Leerplanontwikkeling SLO aan.
+   <br><br>Het bedrijf achter EduStory is Pixel Monkeys, die bestaat uit 5 web designers uit Groningen.
+   Bij Pixel Monkeys draait een product om inhoud, vormgeving en een positieve user experience.</p>
     </div>
 </div>
+<div class="container">
+    <h2>Hallo reiziger!</h2>
+    <p>Welkom bij EduStory: het Hanze avontuur.<br> Ben je nieuw hier of ben je hier al eerder geweest?</p>
 @guest
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-</li>
-<li class="nav-item">
+<div class="buttons">
+
+    <a class="nav-link" href="{{ route('login') }}"><button class="nav-item">{{ __('Inloggen') }}</button></a>
+
+
     @if (Route::has('register'))
-        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        <a class="nav-link" href="{{ route('register') }}"><button class="nav-item">{{ __('Registreren') }}</button></a>
     @endif
-</li>
+
+</div>
 @else
 <li class="nav-item">
     <a class="nav-link" href="/chapters/prologue">{{ __('Proloog') }}</a>
@@ -39,6 +53,7 @@
     </div>
 </li>
 @endguest
+</div>
 <script src="{{ asset('js/infomodal.js') }}"></script>
 @section('scripts')
 
