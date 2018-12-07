d082874d6fcbd1daeb7c2f1eea42bd0c8f482b54
Anna Blaauw <annabblaauw@gmail.com>
2018-12-07 16:03:55 +0100

    update setsail text

--- resources/views/story/0/continue.blade.php
+++ resources/views/story/0/continue.blade.php
@@ -1,5 +1,5 @@
-<p>Dus je heet {{ Auth::user()->name }} en je bent {{ Auth::user()->age }} jaar oud. Welkom!</p>
+<p>Dus je heet {{ Auth::user()->name }} en je bent {{ Auth::user()->age }} jaar oud.</p>
 
-<p>Hier komt de rest van de proloog.</p>
+<p>Welkom bij onze bemanning! De volgende bestemming is Lubeck. Hijs de zeilen!</p>
 
-<p><a href="/chapters/prologue/setsail">Vaar naar Lubek</a></p>
+<p><a href="/chapters/prologue/setsail">Vaar naar Lubeck</a></p>
--- resources/views/story/0/setsail.blade.php
+++ resources/views/story/0/setsail.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts/chapter')
 
 @section('content')
-<p>Hier komt het verhaal van wat er aan boord van het schip gebeurt op de reis naar Lubek.</p>
+<p>"Wat een wind he?" roept Jan, terwijl hij je het schip rondleidt. Je ziet Groningen steeds kleiner worden terwijl jullie wegvaren. "Ben je al eens eerder in Lubeck geweest?"
+    "Ik geloof van niet" antwoord je. Jan houdt maar niet op met praten. "Lubeck is de plaats waar onze handelssamenwerking is onstaan!
+    Al vanaf de 12e eeuw hebben ze economische verdragen gemaakt met andere steden. Heel goed voor de handel. Daarom doen wij er als Groningers ook aan mee. Ze kopen hier onder andere heel erg veel stoffen in. 
+    De "Koningin van de Hanze" noemen ze deze stad ook wel."
+</p>
 
-<p><a href="/chapters/1">Arriveer in Lubek</a></p>
+<p><a href="/chapters/1">Arriveer in Lubeck</a></p>
 @endsection
