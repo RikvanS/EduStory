db7d154aadbe1d815c386a4e3710a6fdee686c99
Anna Blaauw <annabblaauw@gmail.com>
2018-12-13 13:39:28 +0100

    update setsails

--- resources/views/story/1/setsail1b.blade.php
+++ resources/views/story/1/setsail1b.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
             <h2>Onderweg naar Gdansk</h2>
-    <p class="sailtext">"Wat een wind he?" roept Jan, terwijl hij je het schip rondleidt. Je ziet Groningen steeds kleiner worden terwijl jullie wegvaren. "Ben je al eens eerder in Lubeck geweest?"
-            "Ik geloof van niet" antwoord je. Jan houdt maar niet op met praten. "Lubeck is de plaats waar onze handelssamenwerking is onstaan!
-            Al vanaf de 12e eeuw hebben ze economische verdragen gemaakt met andere steden. Heel goed voor de handel. Daarom doen wij er als Groningers ook aan mee. Ze kopen hier onder andere heel erg veel stoffen in.
-            De "Koningin van de Hanze" noemen ze deze stad ook wel."</p>
+    <p class="sailtext">
+         Je bent weer aan boord van het schip, ditmaal beneden het dek. Je bent net in gedachten verzonken, wanneer Jan alweer naast je komt zitten. 
+         “Gdansk! Wat een gekke naam he? We noemen het ook wel Danzig hoor, in Holland. Ondanks de Duitstalige overheersing, wonen er hier mensen vanuit heel Europa. 
+         Scandinavische, Engelse en Schotse kooplieden! Handig om je talen te oefenen. <i> Willkommen in Gdansk! </i> ”
+    </p>
     <a href="/chapters/2b"> <button type="button" class="continue">Arriveer in Gdansk</button></a> 
-        </div>
+    </div>
 @endsection
