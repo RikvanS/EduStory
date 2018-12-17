928c5810bfcad8de7c90d55aee8ba51a7ff0b76d
Dorieke Berends <dorieke90@gmail.com>
2018-12-17 16:51:20 +0100

    Compressed riga.jpg

--- resources/views/story/0/prologue.blade.php
+++ resources/views/story/0/prologue.blade.php
@@ -6,10 +6,10 @@
 
 <div>
     <p>
-     "wat een drukte hier!" zeg je tegen jezelf. Je staat te wachten bij de A-brug in het centrum van Groningen. 
-     Fietsers, auto's en stadsbussen rijden allemaal in rap tempo langs je. Je vindt eindelijk een rustig moment om over te steken. 
-     Langs het water lopend verwonder je je over de gebouwen waar je langs loopt, ze zien er oud uit. Een balk steekt uit de bovenste verdieping van een pakhuis. 
-     Je kijkt omhoog, en let niet op waar je loopt. Je stapt zo in het water!
+     "Wat een drukte hier!" zeg je tegen jezelf. Je staat te wachten bij de A-brug in het centrum van Groningen. 
+     Fietsers, auto's en stadsbussen rijden allemaal in rap tempo langs je heen. Je vindt eindelijk een rustig moment om over te steken. 
+     Langs het water lopend verwonder je je over de gebouwen om je heen, ze zien er oud uit. Een balk steekt uit de bovenste verdieping van een pakhuis. 
+     Je kijkt omhoog, en let niet op waar je loopt. Dan val je zomaar in het water!
     </p>
 </div>
 
@@ -19,10 +19,10 @@
 
 <div>
     <p>
-      Wat een schrik! het duurt een moment voordat je doorhebt dat je gewoon in het water gevallen bent. Je komt boven water en opent je ogen weer. Je ziet de kade, en zwemt die kant op. 
+      Wat een schrik! Het duurt even voordat je doorhebt dat je gewoon in het water gevallen bent. Je komt boven water en opent je ogen weer. Je ziet de kade, en zwemt die kant op. 
       Gelukkig steekt iemand zijn hand uit, en die tilt je de kade op. De man lijkt totaal anders dan wat je bent gewend. Zijn kleding lijkt uit een toneelstuk te komen. 
       Je bent nog wel aan de A kade, maar het lijkt anders dan eerder. Wat is er aan de hand? Alles lijkt net uit een film, of heel ouderwets.
-      De grote pakhuizen zien er nieuw uit. Er worden goederen in en uit geladen. Je ziet zakken met graan, wol en huiden voorbij komen. Het komt uit hele ouderwetse boten, niet de vrachtwagens die je normaal bij de winkel ziet.
+      De grote pakhuizen zien er nieuw uit, er worden goederen in- en uitgeladen. Je ziet zakken met graan, wol en huiden voorbij komen. Het komt uit hele ouderwetse boten, niet uit de vrachtwagens die je hier normaal gesproken ziet laden en lossen.
       Je loopt iets verder en je komt bij het Reitdiep. Ook hier zijn mensen overal bezig, en ze lijken nog steeds verkleed te zijn. Er komt een vrouw met een kind op de arm naar je toegelopen, ze reikt je een jas aan omdat je er zo koud en nat uitziet.
       Je bedankt haar en loopt een van de pakhuizen binnen om op te warmen. De inrichting is anders dan je gewend bent, maar je kan goed zien dat dit een gebouw is van mensen die goed verdienen. Er hangen schilderijen aan de muur.
     </p> 
