d0f42a6d2c9c8db1527c7fe5e21c110996980ed0
Dorieke Berends <dorieke90@gmail.com>
2018-12-18 16:24:02 +0100

    Finished Lubeck story

--- resources/views/story/2/chapter2a.blade.php
+++ resources/views/story/2/chapter2a.blade.php
@@ -21,8 +21,8 @@
 
     
 
- <p> Terwijl je over de horizon tuurt, schrik je van de bellen die opeens aan boord overal op het dek klinken.
-  Met de haven van Visby in zicht, worden jullie toch nog verrast door een aanval van piraten!
+ <p> Terwijl je over de horizon tuurt, schrik je van de bellen die opeens aan boord overal op het dek klinken.</p>
+  <p>Met de haven van Visby in zicht, worden jullie toch nog verrast door een aanval van piraten!
   Verdedig jezelf door je vloot goed te plaatsen, en daarna de locatie van de boten van de piraat te vinden! 
   De rode stippen geven aan dat je de boot van de tegenstander hebt geraakt. Wie als eerste alle boten van de ander heeft laten zinken, heeft gewonnen.</p>
 <div id="main">
@@ -56,7 +56,7 @@
 <div class='section-progress-div' id="sub-content" style="display:none;">
 
 <p>Op het nippertje zijn jullie aan de piraten ontkomen en varen jullie snel de haven van Visby binnen. Terwijl de vaste bemannening begint met hun taken, neemt Jan je mee. 
-  "Komop  {{ Auth::user()->name }}" zegt Jan, "We zijn hier niet voor niks! We moeten nog scheepshuiden kopen voor onze handel in Riga." Samen met je reisgenoot loop je door de mensen, totdat Jan een bekend gezicht ziet. 
+  "Komop  {{ Auth::user()->name }}" zegt Jan, "We zijn hier niet voor niks! We moeten nog schaapshuiden kopen voor onze handel in Riga." Samen met je reisgenoot loop je door de mensen, totdat Jan een bekend gezicht ziet. 
   "Lars!" roept Jan uit. Een lange, blonde man draait zich om en begroet Jan met een flinke handdruk. Daarna kijkt Lars naar jou, pakt je hand en schud hem zo stevig dat je je hand bijna geplet voelt worden. 
   Jan ziet het en geeft je met een schaterlach nog een flinke schouderklop toe. "Bij handelen hoort een stevige handdruk!" legt hij uit. </p>
 <p>Terwijl jullie de schaapshuiden naar de boot brengen, komt er een jongen nieuwsgierig bij jullie kijken. 
