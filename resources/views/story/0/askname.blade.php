5fd46b492c5eb5c79c1feece6a158618d37daaca
Falco Rodenburg <falcorodenburg@gmail.com>
2018-12-20 09:16:37 +0100

    store changes

--- resources/views/story/0/askname.blade.php
+++ resources/views/story/0/askname.blade.php
@@ -1,4 +1,4 @@
 <p>Wat is jouw naam?</p>
 @csrf
-<input id="name" name="name" type="text" class="text">
+<input id="name" name="name" type="text" class="input-text">
 <button onclick="loadDoc('POST', '/chapters/prologue/checkname', changeDiv, 'change-div',  'name')">Bevestig</button>
--- resources/views/story/1/artdealer.blade.php
+++ resources/views/story/1/artdealer.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <link rel="stylesheet" href="{{ URL::asset('css/artgallery.css') }} ">
-<img src="{{URL::asset('/images/lubeck/artdealer-min.jpg')}}">
+<div id="artdeal">
+</div>
 
 
 <br>
