56ca0afd0b85e0b10f67c924314bf45df59e522d
Rik <Rikvstraaten@gmail.com>
2018-12-20 14:25:01 +0100

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{ URL::asset('css/404.css') }}">
+<link href="https://fonts.googleapis.com/css?family=MedievalSharp|Open+Sans|Almendra|Merriweather" rel="stylesheet"> 
+<div class="errorhandler">Je bent verdwaald!</div>
