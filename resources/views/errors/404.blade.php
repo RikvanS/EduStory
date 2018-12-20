fab80a97c00f40b04b16bfe9e731e4ca8f13b3a8
Rik <Rikvstraaten@gmail.com>
2018-12-20 14:26:28 +0100

    Made 404 text clearer

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="{{ URL::asset('css/404.css') }}">
 <link href="https://fonts.googleapis.com/css?family=MedievalSharp|Open+Sans|Almendra|Merriweather" rel="stylesheet"> 
-<div class="errorhandler">Je bent verdwaald!</div>
+<div class="errorhandler">Je bent verdwaald, keer om!</div>
